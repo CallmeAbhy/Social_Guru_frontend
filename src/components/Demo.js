79d664597be18a44542fa824d59ccc241f25eb96
CallmeAbhy <99988396+CallmeAbhy@users.noreply.github.com>
2023-02-04 23:08:35 +0530

    update

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,33 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
-import signUp from './Organization'
-function Demo() {
-  const [show, setShow] = useState(false);
+import axios from 'axios';
 
+function Demo(props) {
+ 
+  const [show, setShow] = useState(false);
+  
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
+  const verify = (event) => {
+    const email = props.email; 
+    const otp = event.target.otp.value;
+    const store = {email,otp};
+    console.log(store);
+    axios.post("http://34.205.65.36:4000/client/verify",store).then(response => {
+      console.log(response);
+      event.target.reset();
+      window.alert("welcome")
+    
+    })
+    .catch(error => {
+      console.log(error);
+      window.alert("error")
+    })
+  };
   return (
     <>
-      <Button bg="dark" onclick={signUp}>Verfify</Button>
-      <Button bg="dark" onClick={handleShow}>Register</Button>
+      
+      <button type="submit" bg="dark" onClick={handleShow}>Register</button>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>OTP Verification</Modal.Title>
         </Modal.Header>
         <Modal.Body>
         <h7>Your OTP is Sent Successfully to your Registered Email Id</h7>
-<Form>
+<Form onSubmit={verify} method='POST'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Enter Your OTP</Form.Label>
-        <Form.Control type="integer" placeholder="Enter Your 4 digit Otp" />
+        <Form.Control type="integer" placeholder="Enter Your 4 digit Otp" name="otp" required />
         <Form.Text className="text-muted">
           Do not share your OTP with anyone else.
         </Form.Text>
       </Form.Group>
-      <Button variant="dark" type="submit">
+      <Button variant="dark">
         Resend OTP
       </Button>
 </Form>
@@ -36,7 +53,7 @@ function Demo() {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="dark" onClick={handleClose}>
+          <Button variant="dark" type='submit'>
             Submit
           </Button>
         </Modal.Footer>
